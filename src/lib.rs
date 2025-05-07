f85036c7b31a5f49f17511a103be38cfdceccc73
Jeryn Aldaron Lau <aldaronlau@gmail.com>
2025-05-07 00:50:01 -0500

    Add `cast_identity_ref()` and `cast_identity_mut()`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Specialization
+//! Safe specialization on stable Rust with builder-like pattern 
 
 #![allow(dead_code)]
 
@@ -91,4 +91,54 @@ where
     (&mut Some(ty) as &mut dyn Any)
         .downcast_mut::<Option<U>>()?
         .take()
+}
+
+/// Attempt to cast `&T` to `&U`.
+///
+/// Returns `None` if they are not the same type.
+///
+/// ```rust
+/// fn only_string<T: 'static>(t: &T) -> Option<&String> {
+///     specializer::cast_identity_ref::<T, String>(t)
+/// }
+///
+/// assert!(only_string(&()).is_none());
+/// assert!(only_string(&1).is_none());
+/// assert!(only_string(&"Hello").is_none());
+/// assert_eq!(
+///     only_string(&"Hello".to_string()).map(|x| x.as_str()),
+///     Some("Hello"),
+/// );
+/// ```
+pub fn cast_identity_ref<T, U>(ty: &T) -> Option<&U>
+where
+    T: 'static,
+    U: 'static,
+{
+    (ty as &dyn Any).downcast_ref::<U>()
+}
+
+/// Attempt to cast `&mut T` to `&mut U`.
+///
+/// Returns `None` if they are not the same type.
+///
+/// ```rust
+/// fn only_string<T: 'static>(t: &mut T) -> Option<&mut String> {
+///     specializer::cast_identity_mut::<T, String>(t)
+/// }
+///
+/// assert!(only_string(&mut ()).is_none());
+/// assert!(only_string(&mut 1).is_none());
+/// assert!(only_string(&mut "Hello").is_none());
+/// assert_eq!(
+///     only_string(&mut "Hello".to_string()),
+///     Some(&mut "Hello".to_string()),
+/// );
+/// ```
+pub fn cast_identity_mut<T, U>(ty: &mut T) -> Option<&mut U>
+where
+    T: 'static,
+    U: 'static,
+{
+    (ty as &mut dyn Any).downcast_mut::<U>()
 }
