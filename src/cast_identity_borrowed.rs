0a02f18b5e78b7331895307315d66da3f2f5c72f
Jeryn Aldaron Lau <aldaronlau@gmail.com>
2025-06-12 18:34:50 -0500

    Implement `CastIdentityBorrowed` for tuples with up to 3 elements

--- src/cast_identity_borrowed.rs
+++ src/cast_identity_borrowed.rs
@@ -200,6 +200,151 @@ where
     }
 }
 
+impl<T, U> CastIdentityBorrowed<(U,)> for (T,)
+where
+    T: CastIdentityBorrowed<U>,
+{
+    fn cast_identity(self) -> Option<(U,)> {
+        let (a,) = self;
+
+        Some((crate::cast_identity_borrowed(a)?,))
+    }
+
+    #[inline(always)]
+    fn is_same() -> bool {
+        <T as CastIdentityBorrowed<U>>::is_same()
+    }
+}
+
+impl<T, U, V, W> CastIdentityBorrowed<(U, W)> for (T, V)
+where
+    T: CastIdentityBorrowed<U>,
+    V: CastIdentityBorrowed<W>,
+{
+    fn cast_identity(self) -> Option<(U, W)> {
+        let (a, b) = self;
+
+        Some((
+            crate::cast_identity_borrowed(a)?,
+            crate::cast_identity_borrowed(b)?,
+        ))
+    }
+
+    #[inline(always)]
+    fn is_same() -> bool {
+        <T as CastIdentityBorrowed<U>>::is_same()
+            && <V as CastIdentityBorrowed<W>>::is_same()
+    }
+}
+
+impl<T, U, V, W, X, Y> CastIdentityBorrowed<(U, W, Y)> for (T, V, X)
+where
+    T: CastIdentityBorrowed<U>,
+    V: CastIdentityBorrowed<W>,
+    X: CastIdentityBorrowed<Y>,
+{
+    fn cast_identity(self) -> Option<(U, W, Y)> {
+        let (a, b, c) = self;
+
+        Some((
+            crate::cast_identity_borrowed(a)?,
+            crate::cast_identity_borrowed(b)?,
+            crate::cast_identity_borrowed(c)?,
+        ))
+    }
+
+    #[inline(always)]
+    fn is_same() -> bool {
+        <T as CastIdentityBorrowed<U>>::is_same()
+            && <V as CastIdentityBorrowed<W>>::is_same()
+            && <X as CastIdentityBorrowed<Y>>::is_same()
+    }
+}
+
+impl<T, U> CastIdentityBorrowed<&mut T> for (U,) {}
+
+impl<T, U> CastIdentityBorrowed<&T> for (U,) {}
+
+impl<T, U> CastIdentityBorrowed<Pin<&mut T>> for (U,) {}
+
+impl<T, U> CastIdentityBorrowed<Pin<&T>> for (U,) {}
+
+impl<T, U> CastIdentityBorrowed<Option<T>> for (U,) {}
+
+impl<T, U> CastIdentityBorrowed<Poll<T>> for (U,) {}
+
+impl<T, U, E> CastIdentityBorrowed<Result<T, E>> for (U,) {}
+
+impl<T, U, V> CastIdentityBorrowed<&mut T> for (U, V) {}
+
+impl<T, U, V> CastIdentityBorrowed<&T> for (U, V) {}
+
+impl<T, U, V> CastIdentityBorrowed<Pin<&mut T>> for (U, V) {}
+
+impl<T, U, V> CastIdentityBorrowed<Pin<&T>> for (U, V) {}
+
+impl<T, U, V> CastIdentityBorrowed<Option<T>> for (U, V) {}
+
+impl<T, U, V> CastIdentityBorrowed<Poll<T>> for (U, V) {}
+
+impl<T, U, V, E> CastIdentityBorrowed<Result<T, E>> for (U, V) {}
+
+impl<T, U, V, W> CastIdentityBorrowed<&mut T> for (U, V, W) {}
+
+impl<T, U, V, W> CastIdentityBorrowed<&T> for (U, V, W) {}
+
+impl<T, U, V, W> CastIdentityBorrowed<Pin<&mut T>> for (U, V, W) {}
+
+impl<T, U, V, W> CastIdentityBorrowed<Pin<&T>> for (U, V, W) {}
+
+impl<T, U, V, W> CastIdentityBorrowed<Option<T>> for (U, V, W) {}
+
+impl<T, U, V, W> CastIdentityBorrowed<Poll<T>> for (U, V, W) {}
+
+impl<T, U, V, W, E> CastIdentityBorrowed<Result<T, E>> for (U, V, W) {}
+
+impl<T, U> CastIdentityBorrowed<(U,)> for &mut T {}
+
+impl<T, U> CastIdentityBorrowed<(U,)> for &T {}
+
+impl<T, U> CastIdentityBorrowed<(U,)> for Pin<&mut T> {}
+
+impl<T, U> CastIdentityBorrowed<(U,)> for Pin<&T> {}
+
+impl<T, U> CastIdentityBorrowed<(U,)> for Option<T> {}
+
+impl<T, U> CastIdentityBorrowed<(U,)> for Poll<T> {}
+
+impl<T, U, E> CastIdentityBorrowed<(U,)> for Result<T, E> {}
+
+impl<T, U, V> CastIdentityBorrowed<(U, V)> for &mut T {}
+
+impl<T, U, V> CastIdentityBorrowed<(U, V)> for &T {}
+
+impl<T, U, V> CastIdentityBorrowed<(U, V)> for Pin<&mut T> {}
+
+impl<T, U, V> CastIdentityBorrowed<(U, V)> for Pin<&T> {}
+
+impl<T, U, V> CastIdentityBorrowed<(U, V)> for Option<T> {}
+
+impl<T, U, V> CastIdentityBorrowed<(U, V)> for Poll<T> {}
+
+impl<T, U, V, E> CastIdentityBorrowed<(U, V)> for Result<T, E> {}
+
+impl<T, U, V, W> CastIdentityBorrowed<(U, V, W)> for &mut T {}
+
+impl<T, U, V, W> CastIdentityBorrowed<(U, V, W)> for &T {}
+
+impl<T, U, V, W> CastIdentityBorrowed<(U, V, W)> for Pin<&mut T> {}
+
+impl<T, U, V, W> CastIdentityBorrowed<(U, V, W)> for Pin<&T> {}
+
+impl<T, U, V, W> CastIdentityBorrowed<(U, V, W)> for Option<T> {}
+
+impl<T, U, V, W> CastIdentityBorrowed<(U, V, W)> for Poll<T> {}
+
+impl<T, U, V, W, E> CastIdentityBorrowed<(U, V, W)> for Result<T, E> {}
+
 impl<'a, T, U> CastIdentityBorrowed<&'a U> for &'a mut T {}
 
 impl<'a, T, U> CastIdentityBorrowed<Pin<&'a U>> for &'a mut T {}
